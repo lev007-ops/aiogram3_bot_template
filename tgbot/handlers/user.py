805c4433c03f0fa07c57864f5824adf78cc5fdcc
Lev <lev_bariakh@icloud.com>
2024-03-23 10:31:55 +0500

    update requirements

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,5 +1,3 @@
-# This file is for demonstration. You need to remove it for production
-
 from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
@@ -9,4 +7,4 @@ user_router = Router()
 
 @user_router.message(CommandStart())
 async def user_start(message: Message):
-    await message.reply("Hello, user!")
+    await message.reply("Hello!")
